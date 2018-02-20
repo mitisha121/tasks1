ae2796b89e654e9105f8a8db38a2e4a0d00395d0
mitisha121 <dodderi.m@husky.neu.edu>
2018-02-20 00:41:29 -0500

    fix

--- lib/tasks1_web/router.ex
+++ lib/tasks1_web/router.ex
@@ -12,12 +12,9 @@ defmodule Tasks1Web.Router do
 
   def get_current_user(conn, params) do
     user_id = get_session(conn, :user_id)
-    if user_id do
-      user = Tasks1.Accounts.get_user!(user_id)
-      assign(conn, :current_user, user)
-    else
-      assign(conn, :current_user, nil)
-    end
+    user = Tasks1.Accounts.get_user(user_id || -1)
+    assign(conn, :current_user, user)
+    
   end
 
   pipeline :api do
